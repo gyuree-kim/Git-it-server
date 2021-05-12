12a7f7d6b8e212b9c4a6507977a4f0c958d0c5b0
2019000982 <2019000982@hanyang.ac.kr>
2021-05-12 14:47:30 +0900

    feat: edit user schema

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const User = required('./user')
+
 const user = new Schema({
   userName: {type: String, unique: true, required: true},
   tier: {type: String},
@@ -8,7 +10,7 @@ const user = new Schema({
   totalCommits: {type: Number},
   average: {type: Number},
   streak: {type: Number},
-  friends: [{type: user}]
+  friends: [{type: Schema.Types.String, ref: 'User'}]
 });
 
 module.exports = mongoose.model('user', user);
