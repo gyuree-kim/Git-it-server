515ad62c0415c1a460edca278ca3a9e277304166
jiwonlee <2019000982@hanyang.ac.kr>
2021-05-14 22:58:44 +0900

    feat: edit schema

--- models/user.js
+++ models/user.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const User = require('./user')
-
 const user = new Schema({
-  userName: {type: String, unique: true, required: true},
-  tier: String,
-  school: String,
-  totalCommits: Number,
-  average: Number,
-  streak: Number,
+  userName: {type: String, unique: true, required: true}, // user name
+  tier: String, // tier by number of commits
+  school: String, // school
+  totalCommits: Number, // commits for a year
+  average: Number,  // totalCommits/365
+  streak: Number, // consecutive days
   friends: [this],
-  rank: Number,
-  image: { data: Buffer, contentType: String}
+  rank: Number, // rank of shcool
+  image: { data: Buffer, contentType: String} // profile image
 });
 
 module.exports = mongoose.model('User', user);
