a78e75421f10eb0f7d80a6f19e1f077f9a623241
Rudolf0328 <54528011+Rudolf0328@users.noreply.github.com>
2021-05-26 15:40:26 +0900

    update user schema (#2)
    * feat: edit user schema
    
    * feat: edit schema
    
    * feat: edit schema
    
    Co-authored-by: 2019000982 <2019000982@hanyang.ac.kr>

--- models/user.js
+++ models/user.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const user = new Schema({
-  userName: {type: String, unique: true, required: true},
-  tier: {type: String},
-  school: {type: String},
-  totalCommits: {type: Number},
-  average: {type: Number},
-  streak: {type: Number},
-  friends: [{type: user}]
+  userName: {type: String, unique: true, required: true}, // user name
+  tier: String, // tier by number of commits
+  school: String, // school
+  totalCommits: Number, // commits for a year
+  average: Number,  // totalCommits/365
+  streak: Number, // consecutive days
+  friends: [this],
+  rank: Number, // rank of shcool
+  image: { data: Buffer, contentType: String} // profile image
 });
 
-module.exports = mongoose.model('user', user);
+module.exports = mongoose.model('User', user);
